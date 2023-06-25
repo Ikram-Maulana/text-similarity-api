4fd2fe1c3e42b55be077743535c1d8933a0f0529
Ikram Maulana <ikram075@ummi.ac.id>
2023-06-25 15:05:35 +0700

    Feat: Add Sign In and Sign Out Button Component

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from "@/ui/button";
+import { useToast } from "@/ui/use-toast";
+import { useSignal } from "@preact/signals-react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { signIn } from "next-auth/react";
+
+const SignInButton = () => {
+  const isLoading = useSignal(false);
+  const { toast } = useToast();
+
+  const signInWithGoogle = async () => {
+    try {
+      isLoading.value = true;
+      await signIn("google");
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Error signing out",
+        description: "Please try again later",
+      });
+    }
+  };
+
+  return (
+    <Button onClick={signInWithGoogle} disabled={isLoading.value}>
+      {isLoading.value ? (
+        <>
+          <ReloadIcon className="w-4 h-4 mr-2 animate-spin" />
+          Please Wait
+        </>
+      ) : (
+        "Sign In"
+      )}
+    </Button>
+  );
+};
+
+export default SignInButton;
