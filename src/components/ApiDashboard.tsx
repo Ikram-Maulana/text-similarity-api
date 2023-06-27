d89c7a39da4dadaed57f34b6cac51606fa262ffa
Ikram Maulana <ikram075@ummi.ac.id>
2023-06-27 16:03:49 +0700

    Feat: Add Dashboard UI 2
    Add:
    - Auth logic
    - Check available active API key

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -1,9 +1,52 @@
-import { FC } from "react";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import Heading from "@/ui/heading";
+import { Input } from "@/ui/input";
+import Paragraphs from "@/ui/paragraphs";
+import { formatDistance } from "date-fns";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 
-interface ApiDashboardProps {}
+const ApiDashboard = async () => {
+  const user = await getServerSession(authOptions);
+  if (!user) notFound();
 
-const ApiDashboard: FC<ApiDashboardProps> = ({}) => {
-  return <div>ApiDashboard</div>;
+  const apiKey = await db.apiKey.findMany({
+    where: {
+      userId: user.user.id,
+    },
+  });
+
+  const activeApiKey = apiKey.find((apiKey) => apiKey.enabled);
+  if (!activeApiKey) notFound();
+
+  const userRequests = await db.apiRequest.findMany({
+    where: {
+      apiKeyId: {
+        in: apiKey.map((key) => key.id),
+      },
+    },
+  });
+
+  const serializableRequests = userRequests.map((req) => ({
+    ...req,
+    timestamp: formatDistance(new Date(req.timestamp), new Date()),
+  }));
+
+  return (
+    <div className="container flex flex-col gap-6">
+      <Heading>Welcome back, {user.user.name}</Heading>
+      <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:justify-start">
+        <Paragraphs>Your API key:</Paragraphs>
+        <Input className="truncate w-fit" readOnly value={activeApiKey.key} />
+        {/* Add options to create new / revoke */}
+      </div>
+
+      <Paragraphs className="mt-4 -mb-4 text-center md:text-left">
+        Your API history:
+      </Paragraphs>
+    </div>
+  );
 };
 
 export default ApiDashboard;
