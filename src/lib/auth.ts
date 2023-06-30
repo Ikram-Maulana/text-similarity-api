566461fd1b46e3f78218dbb4c1ab2735821bc19b
Ikram Maulana <ikram075@ummi.ac.id>
2023-06-30 23:00:49 +0700

    Fix: verbose nextauth_secret passed to avoid possible decryption error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,7 +19,6 @@ function getGoogleCredentials(): { clientId: string; clientSecret: string } {
 }
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
