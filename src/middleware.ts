6971ac4ee2f8ae1ee72854f691b3f7393d93616e
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-01 13:20:02 +0700

    Feat: Update Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,7 +42,7 @@ export default withAuth(
 
     if (isAuthPage) {
       if (isAuth) {
-        return NextResponse.redirect("/dashboard");
+        return NextResponse.redirect(new URL("/dashboard", req.url));
       }
       return NextResponse.next();
     }
@@ -51,7 +51,7 @@ export default withAuth(
       !isAuth &&
       sensitiveRoutes.some((route) => pathname.startsWith(route))
     ) {
-      return NextResponse.redirect("/login");
+      return null;
     }
   },
   {
